3694d7d95215d3016e5f6d46e251e1b33de20bf9
samad <samadarshad@hotmail.co.uk>
2021-03-16 17:57:31 +0000

    config for my rds

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "postgres",
+    "password": "nhWi0IxPlWgeHRDadOTx",
+    "database": "postgres",
+    "host": "database-1.cl5acgthoh69.eu-west-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
