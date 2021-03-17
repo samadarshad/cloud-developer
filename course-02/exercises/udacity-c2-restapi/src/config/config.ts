7cf6991bffa78ec75e03ef57e5767a492c4df59e
samad <samadarshad@hotmail.co.uk>
2021-03-17 14:25:10 +0000

    fixing eb

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -4,7 +4,7 @@ export const config = {
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DATABASE,
     "host": process.env.POSTGRES_HOST,
-    "dialect": "postgres",
+    "dialect": process.env.POSTGRES_DIALECT,
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
